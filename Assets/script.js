45fa7534d15f5db349428ba4727ef29e09e779a9
Tolga Secme <tolgasecme@icloud.com>
2021-02-03 17:34:42 -0700

    Fivedaysstartedconsoleout

--- Assets/script.js
+++ Assets/script.js
@@ -11,21 +11,54 @@ function getApi() {
             return data.json();
         }).then(function (data) {
             console.log(data);
+            $("#fetch").empty();
             let today = data;
             let jumbo = document.getElementById("#fetch");
             let infoJumbo = $("<div>");
             let day = $("#fetch-elm").addClass("date-of");
-            let dayOf = $("<p>").addClass("actual-date").text(date + " Weather for: ").append(cityPicked)
-            let degree = $("#temperature").text(Math.round(today.main.temp) + "˚F");
+            let dayOf = $("<p>").addClass("actual-date").text(date);
+            let expCity = $("<p>").addClass("exp-city").text(" Weather for: ").append(cityPicked);
+            let degree = $("<p>").addClass("lead").text(Math.round(today.main.temp) + "˚F");
+            console.log(degree);
             let humid = $("<p>").addClass("humidity").text("Humidity: " + today.main.humidity + "%");
             let wind = $("<p>").addClass("wind").text("Wind Speed: " + Math.round(today.wind.speed) + " mph");
-            $("#fetch").append(infoJumbo.append(day, dayOf, degree, humid, wind));
+            $("#fetch").append(infoJumbo.append(day, dayOf, expCity, degree, humid, wind));
             $("#one-day").append(jumbo);
 
 
         })
 }
 
+function fiveDaysApi() {
+    const urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityPicked + "&units=imperial&appid=45b6598a4a1bd706ba39bf0f2ac2fcf4";
+
+    fetch(urlRequest)
+        .then(function (data) {
+            return data.json();
+        }).then(function (data) {
+            console.log(data);
+            console.log(data.list[0].dt_txt);
+            console.log(data.list[0].main.temp + "˚F");
+            console.log(data.list[0].main.humidity + " %");
+            console.log(data.list[0].wind.speed + " mph");
+            console.log(data.list[8].dt_txt);
+            console.log(data.list[8].main.temp + "˚F");
+            console.log(data.list[8].main.humidity + " %");
+            console.log(data.list[8].wind.speed + " mph");
+            console.log(data.list[16].dt_txt);
+            console.log(data.list[16].main.temp + "˚F");
+            console.log(data.list[16].main.humidity + " %");
+            console.log(data.list[16].wind.speed + " mph");
+            console.log(data.list[24].dt_txt);
+            console.log(data.list[24].main.temp + "˚F");
+            console.log(data.list[24].main.humidity + " %");
+            console.log(data.list[24].wind.speed + " mph");
+            console.log(data.list[32].dt_txt);
+            console.log(data.list[32].main.temp + "˚F");
+            console.log(data.list[32].main.humidity + " %");
+            console.log(data.list[32].wind.speed + " mph");
+        })
+}
 function searchedCities() {
     $("#searched").empty();
     for (let i = 0; i < cityPicked.length; i++) {
@@ -50,9 +83,11 @@ $("#submitBtn").on("click", function (event) {
 
     searchedCities();
     getApi();
+    fiveDaysApi()
     localStorage.setItem("city", JSON.stringify((JSON.parse(localStorage.getItem("city")) || [])));
 
     $("#given-input").val("");
+    $("#issues").css('display', 'block');
 });
 
 searchedCities();
@@ -61,4 +96,5 @@ searchedCities();
 $(document).on("click", ".city", function () {
     cityPicked = $(this).text();
     getApi();
+    fiveDaysApi()
 });
